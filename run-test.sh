dcbdc5a19c82965507416d12b2ab987dfa8c6a14
lenkan <dlenksjo@gmail.com>
2024-07-01 16:13:58 +0200

    re-add pull

--- run-test.sh
+++ run-test.sh
@@ -4,10 +4,10 @@ set -e
 # Clean up
 docker compose down -v
 docker compose build
-# docker compose pull
 
 # Start keria 0.1.2
 export KERIA_IMAGE_TAG=0.1.2
+docker compose pull
 docker compose up -d keria
 
 # Start setup script
@@ -18,9 +18,7 @@ docker compose down keria
 
 # Run migrations with new version
 export KERIA_IMAGE_TAG=0.2.0-dev3
-
-# Disabled pull for now until the new tag is pushed, you have to build weboftrust/keria:0.2.0-dev3 locally to be able to run this test
-# docker compose pull
+docker compose pull
 docker compose run -it --rm --entrypoint /keria/migrate.sh keria
 
 # Start new version
